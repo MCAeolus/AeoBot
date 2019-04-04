759a104f0e90228cfbdf5e71cfd83d26910571d9
Nathan E. S <mcaeolusch@gmail.com>
2019-04-04 15:39:42 -0400

    initial bot commit

--- bot.js
+++ bot.js
@@ -1,2 +1,51 @@
 const discord = require('discord.js');
+const bot = new discord.Client();
+
 const config = require('./conf.json');
+
+const prefix = '%';
+
+const commands = [new TestCommand()];
+
+
+bot.on('ready', () => {
+	
+	console.log("bot running.");
+	bot.user.setActivity("test ( % )");
+	
+});
+
+
+bot.on('message', async msg => {
+	
+	if(msg.author.bot) return;
+	
+	const args = message.content.slice(prefix).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+	
+	for(cmdClass in commands) {
+		if(cmdClass.alias.contains(cmd)) {
+			
+			console.log("Running command " + cmd);
+			cmdClass.run(args, bot, msg);
+			
+		}
+		
+	}
+});
+
+
+bot.login(config.token);
+
+class TestCommand {
+	
+	const alias = ['test','t','try','temp'];
+	
+	function run(args, bot, message) {
+		
+		const send = await msg.channel.send("testing...");
+		m.react(':white_check_mark:');	
+		
+	}
+	
+}
